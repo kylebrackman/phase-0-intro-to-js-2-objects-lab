59e2ae3fa24ef8adc9582c11b328dec65b26e2fa
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-27 00:05:49 -0600

    commit tree 890473623a33d36d572f281569452809e401890c

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-// Write your solution in this file!
+// 2nd practice below
+
+const employee = {
+    name: "kyle",
+    streetAddress: "123 Road"
+}
+
+function updateEmployeeWithKeyAndValue() {
+    const newObject = {...employee}
+    newObject.name = "Sam"
+    newObject.streetAddress = "11 Broadway"
+
+    return newObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey() {
+    const newObject = {...employee}
+    delete newObject.name
+    return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name
+    return employee
+}
+
+// 1st practice below
+// const employee = {
+//     name: "Kyle",
+//     streetAddress: "123 Road"
+// }
+// function updateEmployeeWithKeyAndValue() {
+//     let newEmployee = {...employee}
+//     newEmployee.streetAddress = "11 Broadway"
+//     return newEmployee
+// }
+
+
+
+// function destructivelyUpdateEmployeeWithKeyAndValue() {
+//     employee.name = "Sam"
+//     employee.streetAddress = "12 Broadway"
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey() {
+//     let newEmployee = {...employee};
+//     delete newEmployee.name
+//     return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey() {
+//     delete employee.name
+//     return employee
+// }
+
+// first attempt below
+
+// const employee = {
+//     name: "Kyle",
+//     streetAddress: "Fort Collins",
+// }
+
+// function updateEmployeeWithKeyAndValue() {
+//     const newEmployee = {...employee}
+//     newEmployee.streetAddress = "11 Broadway"
+
+//     return newEmployee
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue() {
+//     employee.streetAddress = "12 Broadway"
+
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey() {
+//     const newEmployee = {...employee}
+//     delete newEmployee.name
+
+//     return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey() {
+//     delete employee.name
+    
+//     return employee
+// }
